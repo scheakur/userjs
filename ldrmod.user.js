1e2849fc86aa6d930ff1bb2fffb9156e2162a5e9
scheakur <scheakur@gmail.com>
2014-07-19 13:21:46 +0900

    For Greasemonkey 2.0

--- ldrmod.user.js
+++ ldrmod.user.js
@@ -4,39 +4,50 @@
 // @include http://reader.livedoor.com/reader/
 // @include http://reader.livedoor.com/public/*
 // @version 0.1.0
+// @grant GM_openInTab
 // ==/UserScript==
 
 (function (w) {
-  function prefetch() {
-    var num = 10;
-    w.get_unread.cache.max = 1000;
-    w.Ordered.list.forEach(function (id) {
-      if (num > 0 && !w.get_unread.cache.has(id) && w.subs_item(id).unread_count) {
-        num--;
-        w.prefetch(id);
+  function ldrmod(w) {
+    function prefetch() {
+      var num = 10;
+      w.get_unread.cache.max = 1000;
+      w.Ordered.list.forEach(function (id) {
+        if (num > 0 && !w.get_unread.cache.has(id) && w.subs_item(id).unread_count) {
+          num--;
+          w.prefetch(id);
+        }
+      });
+    }
+
+    function openInTab() {
+      var item = w.get_active_item(true);
+      if (!item) {
+        return;
       }
+      GM_openInTab(item.link);
+    }
+
+    w.addEventListener('load', function () {
+      w.Keybind.add('m', prefetch);
+      w.Keybind.add('v', openInTab);
     });
   }
 
-  function openInTab() {
-    var item = w.get_active_item(true);
-    if (!item) {
-      return;
-    }
-    setTimeout(function () {
-      GM_openInTab(item.link);
-    }, 0);
+  function contentEval(fn) {
+    var source = ';(' + fn + ')(this);';
+    var script = document.createElement('script');
+    script.setAttribute('type', 'application/javascript');
+    script.textContent = source;
+
+    document.body.appendChild(script);
+    document.body.removeChild(script);
   }
 
-  let onload = function () {
-    w.Keybind.add('m', prefetch);
-    w.Keybind.add('v', openInTab);
-  };
-
-  let _onload = w.onload;
-
-  w.onload = function () {
-    _onload();
-    onload();
-  };
+  exportFunction(function (url) {
+    setTimeout(function () {
+      GM_openInTab(url);
+    }, 0);
+  }, w, {defineAs: 'GM_openInTab'});
+  contentEval(ldrmod);
 }(this.unsafeWindow || this.window));
