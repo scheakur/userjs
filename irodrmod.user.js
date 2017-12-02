35f443e141d61ea9c8a588eb2558c68f4e5792f0
scheakur <scheakur@gmail.com>
2017-12-02 12:22:44 +0900

    Add mod file for irodr

---
+++ irodrmod.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name irodrmod
+// @namespace scheakur.com
+// @include http://irodr.netlify.com/
+// @noframes
+// @version 0.1.0
+// @grant GM_openInTab
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  function openInBackgroundTab() {
+    const content = userScript.getActiveContent();
+
+    if (!content) {
+      return;
+    }
+
+    GM_openInTab(content.url, { active: false });
+  }
+
+  let tryCount = 0;
+
+  function retry(fn) {
+    if (tryCount++ < 10) {
+      setTimeout(() => {
+        fn();
+      }, 33);
+    }
+  }
+
+  function setup() {
+    if (typeof userScript === 'undefined') {
+      retry(setup);
+      return;
+    }
+
+    const unsubscribe = userScript.event.subscribe("SubscriptionContent::componentDidMount", (content) => {
+      userScript.registerKey('v', openInBackgroundTab);
+      unsubscribe();
+    });
+  }
+
+  setup();
+})();
